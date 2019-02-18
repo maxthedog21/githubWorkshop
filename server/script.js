5dd71bf3e848ce22ae9d4b97f7b1e7bc82d2ab1e
Maximilian Moore <maximilianmoore@maximilians-mbp.towson.edu>
2019-02-18 18:13:48 -0500

    front-end

---
+++ server/script.js
@@ -0,0 +1,16 @@
+var convertBtn = document.querySelector('.convert-button');
+var URLinput = document.querySelector('.URL-input');
+
+convertBtn.addEventListener('click', () => {
+
+
+
+    sendUrl(URLinput.value);
+})
+
+function sendURL(URL)
+{
+window.location.href = 'http://localhost:4000/download?URL=${}URL';
+
+
+}
